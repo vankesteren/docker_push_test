4c7e6c6b05519277fe516767611d4376a488100a
Erik-Jan van Kesteren <erikjanvankesteren@pm.me>
2023-12-28 12:46:06 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-echo ">>> welcome to ✨ EJ's magical ✨ alpine container <<<"
+echo ">>> Welcome to ✨ EJ's magical ✨ alpine container <<<"
+echo ">>> Run docker run --rm -ti evankesteren/alpinetest <<<"
+echo ">>>          for an interactive shell               <<<"
 sh
